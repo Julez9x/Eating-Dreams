a6235e3f98c6f11b24fe5c83d22b3e01b7b84c95
xeiya <rrhainier@gmail.com>
2025-03-04 11:48:41 +1100

    Updated Camera
    Camera update

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,6 +8,8 @@ public class CameraFollow : MonoBehaviour
     public float FollowSpeed = 2f;
     //This changes the Y Offset of the camera
     public float yOffset = 1f;
+    //This changes the X Offset
+    public float xOffset = 1f;
     //This gives the position of the player
     public Transform target;
 
@@ -15,7 +17,8 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         //Vector3 because camera is in a 3d space despite being made in a 2d game
-        Vector3 newPos = new Vector3(target.position.x, target.position.y + yOffset, -10f);
+        Vector3 newPos = new Vector3(target.position.x + xOffset, target.position.y + yOffset, -10f);
+
         //Slerp interpolates two Vectors within a sphereical radius
         transform.position = Vector3.Slerp(transform.position, newPos, FollowSpeed * Time.deltaTime);
 
